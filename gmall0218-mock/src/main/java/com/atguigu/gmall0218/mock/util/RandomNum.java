9a84267444f884bc70f438253e0c13a1cab0546c
zhangchen <windyzj@163.com>
2019-07-19 11:46:45 +0800

    Initial commit

---
+++ gmall0218-mock/src/main/java/com/atguigu/gmall0218/mock/util/RandomNum.java
@@ -0,0 +1,9 @@
+package com.atguigu.gmall0218.mock.util;
+
+import java.util.Random;
+
+public class RandomNum {
+    public static final  int getRandInt(int fromNum,int toNum){
+        return   fromNum+ new Random().nextInt(toNum-fromNum+1);
+    }
+}
